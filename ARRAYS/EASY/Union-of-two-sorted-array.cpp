bcccb2084243c5e55205d9d995f0723146faa3ae
atetoon <i.am.dhruvpandey@gmail.com>
2026-08-08 22:51:59 +0530

    add optimal approach for union two sorted arrays problem

--- ARRAYS/EASY/Union-of-two-sorted-array.cpp
+++ ARRAYS/EASY/Union-of-two-sorted-array.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+//brute force
 vector<int> unionArray(vector<int> &arr1, vector<int> &arr2, int n1, int n2) {
     set<int> st;
 
@@ -20,6 +21,44 @@ vector<int> unionArray(vector<int> &arr1, vector<int> &arr2, int n1, int n2) {
     return uni;
 }
 
+//optimal
+vector<int> uni2(vector<int> &a, vector<int> &b) {
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0;
+    int j = 0;
+    vector<int> unionArr;
+    while(i<n1 && j<n2) {
+        if(a[i] <= b[j]) {
+            if(unionArr.size() == 0 || unionArr.back()!= a[i]) {
+                unionArr.push_back((a[i]));
+            }
+            i++;
+        } else {
+            if(unionArr.size() == 0 || unionArr.back()!= b[j]) {
+                unionArr.push_back((b[j]));
+            }
+            j++;
+        }
+    }
+
+    while(j<n2) {
+        if(unionArr.size() == 0 || unionArr.back()!= b[j]) {
+            unionArr.push_back((b[j]));
+        }
+        j++;
+    }
+
+    while(i<n1) {
+        if(unionArr.size() == 0 || unionArr.back()!= a[i]) {
+            unionArr.push_back((a[i]));
+        }
+        i++;
+    }
+
+    return unionArr;
+}
+
 int main() {
     int n1, n2;
     cin >> n1 >> n2;
@@ -27,6 +66,6 @@ int main() {
     vector<int> myArr2(n2);
     for(int i = 0; i<n1; i++) cin >> myArr1[i];
     for(int i = 0; i<n2; i++) cin >> myArr2[i];
-    vector<int> v = unionArray(myArr1, myArr2, n1, n2);
+    vector<int> v = uni2(myArr1, myArr2);
     for(int i = 0;i<v.size(); i++) cout << v[i] << " ";
 }
