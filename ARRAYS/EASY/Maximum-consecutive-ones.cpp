b7db5bf8e1fd7ebb1d3ba5501bff9c9cd34a2f6d
atetoon <i.am.dhruvpandey@gmail.com>
2026-08-09 22:04:33 +0530

    add solution for Maximum consecutive Ones problem

---
+++ ARRAYS/EASY/Maximum-consecutive-ones.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int consecOnes(vector<int> &a, int n) {
+    int cnt=0;
+    int ans = 0;
+
+    for(int i = 0; i<n-1; i++) {
+        if(a[i] == 1) { 
+            cnt++;
+            ans = max(ans, cnt);
+        } else {
+            cnt = 0;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i = 0; i<n; i++) cin>>v[i];
+    cout << consecOnes(v, n);
+    return 0;
+}
