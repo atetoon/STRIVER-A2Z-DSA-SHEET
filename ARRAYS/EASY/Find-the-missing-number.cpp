85289691b08fea78f084b65c977a2753c61e811f
atetoon <i.am.dhruvpandey@gmail.com>
2026-08-09 20:47:28 +0530

    add solution of Find the missing number in the array problem

---
+++ ARRAYS/EASY/Find-the-missing-number.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//brute force
+int missingNum(vector<int> &a, int n) {
+    n = n+1;
+    for(int i = 1; i<=n; i++) {
+        bool found = false;
+        for(int j = 0; j<n-1; j++) {
+            if(a[j] == i) {
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) return i;
+    }
+    return -1;
+}
+
+//better
+int missingNum1(vector<int> &a, int n) {
+    n = n + 1;
+
+    vector<int> hash(n+1, 0);
+    for(int i = 0; i<n-1; i++) {
+        hash[a[i]]++;
+    }
+
+    for(int i = 1; i<=n; i++) {
+        if(hash[i] == 0) return i;
+    }
+
+    return -1;
+}
+
+//optimal
+int missingNum2(vector<int> &a, int n) {
+    n = n + 1;
+    int sum = 0;
+    for(int i = 0; i<n-1; i++) {
+        sum+=a[i];
+    }
+
+    long long expSum = (n *1LL* (n+1))/2;
+    
+    return expSum - sum;
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i<n; i++) cin >> v[i];
+    cout << missingNum1(v, n);
+    return 0;
+}
