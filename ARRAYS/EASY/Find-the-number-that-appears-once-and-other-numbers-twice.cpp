a2c2ccb9c739618ec08c64441a61a4aaee8fdff4
atetoon <i.am.dhruvpandey@gmail.com>
2026-09-03 23:32:50 +0530

    Add multiple implementations to find the number that appears once in an array

--- ARRAYS/EASY/Find-the-number-that-appears-once-and-other-numbers-twice.cpp
+++ ARRAYS/EASY/Find-the-number-that-appears-once-and-other-numbers-twice.cpp
@@ -14,6 +14,43 @@ int numOnce(int n, vector<int> &arr) {
 
     return -1;
 }
+
+//better
+int numOnce1(int n, vector<int> &arr) {
+    int mx = arr[0];            //find maxi element
+    for(int i=0; i<n; i++) mx = max(mx, arr[i]);
+
+    vector<int> hashh(mx, 0);       //hash array
+    for(int i=0; i<n; i++) hashh[arr[i]]++; // frequency
+    
+    for(int i=0; i<n; i++) {
+        if(hashh[arr[i]] == 1) return arr[i];
+    }
+
+    return -1;
+}
+
+//better 2 
+int numOnce2(int n, vector<int> &arr) {
+    map<long long,int> mpp;
+
+    for(int i=0; i<n; i++) {
+        mpp[arr[i]]++;
+    }
+    for(auto it : mpp) {
+        if(it.second == 1) return it.first;
+    }
+    return -1;
+}
+
+//optimal 
+int numOnce3(int n, vector<int> &arr) {
+    int xorr = 0;
+    for(int i=0; i<arr.size(); i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
  
 int main() {
     int n; cin >> n;
@@ -21,6 +58,6 @@ int main() {
     for(int i = 0; i<n; i++) {
         cin >> myArr[i];
     }
-    cout << numOnce(n, myArr) << endl;
+    cout << numOnce3(n, myArr) << endl;
     return 0;
 }
